0b1131c99d0af4fdde0ccc9662337dc59d24a0c0
Adam Abdillah <adamabdillah20@gmail.com>
2025-07-05 12:43:34 +0700

    fixing identityprovider

--- src/voting-app-frontend/src/hooks/useAuth.tsx
+++ src/voting-app-frontend/src/hooks/useAuth.tsx
@@ -114,7 +114,7 @@ export function useAuth(): UseAuthReturn {
 
     try {
       await authClient.login({
-        identityProvider: "http://uxrrr-q7777-77774-qaaaq-cai.localhost:4943/",
+        identityProvider: "https://identity.ic0.app",
         onSuccess: async () => {
           const p = authClient.getIdentity().getPrincipal().toText();
           setPrincipal(p);
